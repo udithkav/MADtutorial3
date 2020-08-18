cd069f651ff304e8066023f1f5b8605dff40a5a0
udithkav <67887799+udithkav@users.noreply.github.com>
2020-08-18 23:41:23 +0530

    Completed Tutorial fixed errors

--- app/src/main/java/com/example/intentsproj/firstActivity.java
+++ app/src/main/java/com/example/intentsproj/firstActivity.java
@@ -2,7 +2,6 @@ package com.example.intentsproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -11,13 +10,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import static android.view.Gravity.LEFT;
-
 public class firstActivity extends AppCompatActivity {
 
     private Button okayButton;
     private EditText number1;
     private EditText number2;
+    private String num1,num2;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +23,20 @@ public class firstActivity extends AppCompatActivity {
         setContentView(R.layout.activity_first);
         number1 =(EditText) findViewById(R.id.number1);
         number2 =(EditText) findViewById(R.id.number2);
-        okayButton = (Button) findViewById(R.id.okButton);
+        okayButton = (Button) findViewById(R.id.plusButton);
+
         okayButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
+                num1 = number1.getText().toString();
+                num2 = number2.getText().toString();
+                Intent intent = new Intent(firstActivity.this,secondActivity.class);
+                intent.putExtra("NUMBER1",num1);
+                intent.putExtra("NUMBER2",num2);
                 Toast toast = Toast.makeText(firstActivity.this,"You pressed OK button",Toast.LENGTH_SHORT);
                 toast.setGravity(Gravity.TOP|Gravity.LEFT, 0, 0);
                 toast.show();
 
-                startActivity(new Intent(firstActivity.this, secondActivity.class));
+                startActivity(intent);
             }
         });
 
--- app/src/main/java/com/example/intentsproj/secondActivity.java
+++ app/src/main/java/com/example/intentsproj/secondActivity.java
@@ -3,12 +3,57 @@ package com.example.intentsproj;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class secondActivity extends AppCompatActivity {
+    private int value1,value2;
+    private int answer;
+    EditText num1,num2;
+    TextView answerText;
 
-    @Override
+    protected void fetchValues(){
+        String val1,val2;
+        val1 = getIntent().getStringExtra("NUMBER1");
+        val2 = getIntent().getStringExtra("NUMBER2");
+        num1.setText(val1);
+        num2.setText(val2);
+        try{
+            value1 = Integer.parseInt(num1.getText().toString());
+            value2 = Integer.parseInt(num2.getText().toString());
+        } catch (NumberFormatException exp){
+            System.out.println(exp);
+        }
+    }
+
+    public void setTextValueAdd(View view){
+        String ans = value1 + "+" + value2  + " = " + (value1+value2);
+        answerText.setText(ans);
+    }
+    public void setTextValueSub(View view){
+        String ans = value1 + "-" + value2  + " = " + (value1-value2);
+        answerText.setText(ans);
+    }
+    public void setTextValueMult(View view){
+        String ans = value1 + "x" + value2  + " = " + (value1*value2);
+        answerText.setText(ans);
+    }
+    public void setTextValueDiv(View view){
+        String ans = value1 + "/" + value2  + " = " + (value1/value2);
+        answerText.setText(ans);
+    }
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        num1 = (EditText) findViewById(R.id.number1);
+        num2 = (EditText) findViewById(R.id.number2);
+        answerText = (TextView) findViewById(R.id.answerText);
+        answerText.setText("Perform Calculation");
+        fetchValues();
+
+
     }
 }
